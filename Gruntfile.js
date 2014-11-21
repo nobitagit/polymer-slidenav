7764fa4670e836e9f48a8d27cb5b003e20d0d77f
Nobita <aurelio.ogliari@gmail.com>
2014-11-21 08:25:38 +0000

    indent

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,43 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        'connect': {
-            demo: {
-                options: {
-                    open: true,
-                    keepalive: true
-                }
-            }
-        },
-        'gh-pages': {
-            options: {
-                base : "" 
-            },
-            src: [
-		            'bower_components/**/*',
-                'demos/*', 
-                'src/*', 
-                'index.html'
-            ]
-        },
-        'replace': {
-            example: {
-                src: ['src/*'],
-                dest: 'dist/',
-                replacements: [{
-                    from: 'bower_components',
-                    to: '..'
-                }]
-            }
+  grunt.initConfig({
+    'connect': {
+      demo: {
+        options: {
+          open: true,
+          keepalive: true
         }
-    });
+      }
+    },
+    'gh-pages': {
+      options: {
+        base : ""
+      },
+      src: [
+        'bower_components/**/*',
+        'demos/*',
+        'src/*',
+        'index.html'
+      ]
+    },
+    'replace': {
+      example: {
+        src: ['src/*'],
+        dest: 'dist/',
+        replacements: [{
+          from: 'bower_components',
+          to: '..'
+        }]
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-gh-pages');
-    grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-text-replace');
 
-    grunt.registerTask('build',  ['replace']);
-    grunt.registerTask('deploy', ['gh-pages']);
-    grunt.registerTask('server', ['connect']);
+  grunt.registerTask('build',  ['replace']);
+  grunt.registerTask('deploy', ['gh-pages']);
+  grunt.registerTask('server', ['connect']);
 
 };
